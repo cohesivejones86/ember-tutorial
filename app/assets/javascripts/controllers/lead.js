1054a58d7cadd94095b8503a3def5c0d80696339
Nate Jones <lemaitre86@gmail.com>
2014-06-15 15:12:29 -0400

    Added delete behaviour to lead

--- app/assets/javascripts/controllers/lead.js
+++ app/assets/javascripts/controllers/lead.js
@@ -6,6 +6,12 @@ App.LeadController = Ember.ObjectController.extend({
   }.property('isDirty', 'isSaving'),
 
   actions: {
+    delete: function() {
+      var self = this;
+      this.get('model').destroyRecord().then(function() {
+            self.transitionToRoute('leads');
+      });
+    },
     saveChanges: function() {
       if (this.get('model.isDirty')) {
         this.get('model').save();
