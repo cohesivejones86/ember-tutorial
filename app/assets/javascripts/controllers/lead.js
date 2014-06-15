0dee0f6956436e29a5cfdf09103528a9c2f36e21
Nate Jones <lemaitre86@gmail.com>
2014-06-15 11:16:04 -0400

    Add is dirty to lead

--- app/assets/javascripts/controllers/lead.js
+++ app/assets/javascripts/controllers/lead.js
@@ -1,7 +1,13 @@
 App.LeadController = Ember.ObjectController.extend({
+  showUnsavedMessage: function() {
+    return this.get('isDirty') && !this.get('isSaving');
+  }.property('isDirty', 'isSaving'),
+
   actions: {
     saveChanges: function() {
-      this.get('model').save();
+      if (this.get('model.isDirty')) {
+        this.get('model').save();
+      }
     }
   }
 });
